852edaaf5930a6bd49d29c8083a12bf63303f4c3
ChaniBlatt <45265488+ChaniBlatt@users.noreply.github.com>
2020-01-15 23:41:26 +0200

    continue  xml parsing for the tests

--- xml_parsing.py
+++ xml_parsing.py
@@ -5,16 +5,41 @@ import xml.etree.cElementTree as et
 file = open("xml_file.xml")
 content = file.read()
 tree=et.fromstring(content)
-dict = [{item.tag: item.text for item in ch} for ch in tree.findall('test')]
-print(dict)
+flowes=tree.findall('flow')
 
 
-#parse list of dictionaries to xml file
-root = et.Element("buttonTests")
+flowes_=[]
 
-for dic in dict:
-    doc = et.SubElement(root, "test")
-    for key in  dic.keys():
-        et.SubElement(doc, key).text = dic[key]
-    tree = et.ElementTree(root)
+for flow in flowes:
+    flow_ = {}
+    tests_ = []
+    for tests in flow:
+        if tests.tag == 'name':
+            flow_['name'] = tests.text
+        else:
+            for test in tests:
+                test_ = {}
+                for i in test:
+                    test_[i.tag] = i.text
+                tests_.append(test_)
+        flow_['tests']=tests_
+    flowes_.append(flow_)
+print(flowes_)
+            # tests = [{item.tag: item.text for item in ch} for test in flow]
+#
+# #parse list of dictionaries to xml file
+root = et.Element("flowes")
+for flow in flowes:
+    doc = et.SubElement(root, "flow")
+    for key in  flow.keys():
+        if key == 'name':
+            et.SubElement(doc, key).text = flow[key]
+        else:
+            doc_=et.SubElement(doc, "tests")
+            for i in flow[key]:
+                test=et.SubElement(doc_, "test")
+                for j in i.keys():
+                    et.SubElement(test, j).text = i[j]
+
+tree = et.ElementTree(root)
 tree.write("output.xml")
